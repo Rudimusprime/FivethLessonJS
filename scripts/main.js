69303cd4829a1515a344c37b7ae99ee04afe780b
DanielB <d.banasiewicz@pdo.com.pl>
2021-09-23 19:58:54 +0200

    Little feature;

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,28 @@
 const input = document.getElementById('pass');
-const password = "user";
-const message = "wyjechałam na zawsze";
+const passwords = ["user", "wiosna", "Ania"];
+const messages = ["wyjechałam na zawsze", "piękna pora roku", "fajny styl masz"];
 const div = document.querySelector('.message')
-input.addEventListener('keyup', (e) => {
-    if (password === e.target.value) {
-        div.textContent = message;
-        e.target.value="";
-    } else {
-        div.textContent = "";
-    }
-    console.log(e.target.value);
-})
 
-input.addEventListener('focus',(e)=>{
-    e.target.classList.add('active');
+input.addEventListener('keyup', (e) => {
+
+    div.textContent = "";
+
+    const text = e.target.value;
+
+    passwords.forEach((password, index) => {
+        if (password === text) {
+            div.textContent = messages[index];
+            e.target.value = "";
+        }
+    })
 })
-input.addEventListener('blur',(e)=>{
+input.addEventListener('focus', (e) => {
+
+    e.target.classList.add('active');
+
+})
+input.addEventListener('blur', (e) => {
+
     e.target.classList.remove('active');
+
 })
